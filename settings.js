b80a16f24008652cf174e1309dc7433c8d3f33e1
dilzstudio <108262302+dilzstudio@users.noreply.github.com>
2022-06-27 13:49:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,27 +23,27 @@ global.APIKeys = {
 global.alivemsg = `My name is RED DRAGON🔥
 My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94741630677'] //Owner number in aive msg
+global.premium = ['+94741630677'] //Owner Number info
+global.ownernomer = '+94741630677' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON' //Bot Name
+global.ownername = 'DIL_ZHAN-STUDIO' //Owner Name
+global.botname = ' ☬YEZ-DEVILL' //Bot Name
 
 global.button = '🤭මේක ටච් කරන්න📍' //Costomize A Button Name In Alive Message
 global.btnurl = '🙂🔥දැන් සැපද?😑' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬DIL_ZHAN-STUDIO࿐' //Bot Pacage Name
+global.author = '😎DIL_ZHAN-STUDIO❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor In Menu!
 
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
-global.footer = '©_Kumuthu.' // DO NOT CHANGE!
+global.ig = 'https://www.youtube.com/channel/UCoWps7QbzxB5FOgTEMli7Vw' //Github Account!
+global.footer = '©_DIL_ZHAN-STUDIO.' // DO NOT CHANGE!
 
 global.mess = { // Error Mesages
     success: 'Done ✅',
